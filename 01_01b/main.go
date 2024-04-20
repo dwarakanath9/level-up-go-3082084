179d57521049102034b80e55dc1c1acd94266a4c
dwarakanath9 <dwarakanathc9@gmail.com>
2024-04-20 17:59:16 +0000

    first example

--- 01_01b/main.go
+++ 01_01b/main.go
@@ -22,9 +22,12 @@ func parseTime(target string) time.Time {
 // calcSleeps returns the number of sleeps until the target.
 func calcSleeps(target time.Time) float64 {
 	fmt.Println("current time ", time.Now(), "target is :", target)
-	dt := target.Sub(time.Now())
-	fmt.Println(" the duration :", dt)
-	return dt.Hours() / 24
+	dt := target.Sub(time.Now()) / 24
+	dt1 := time.Until(target).Hours() / 24
+	fmt.Println(" the until function returns", dt1)
+	fmt.Println(" the sub function returns", dt)
+
+	return dt1
 }
 
 func main() {
